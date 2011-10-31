cd8557155f381243307585da6d863992cf2dd903
Nito Martinez <nito@cpan.org>
2011-10-31 05:50:11 +0000

    Import of NITO/Net-SugarCRM-1.12816 from CPAN.
    gitpan-cpan-distribution: Net-SugarCRM
    gitpan-cpan-version:      1.12816
    gitpan-cpan-path:         NITO/Net-SugarCRM-1.12816.tar.gz
    gitpan-cpan-author:       NITO
    gitpan-cpan-maturity:     released

---
+++ t/lib/defaults.pl
@@ -0,0 +1,16 @@
+#!perl
+package Test;
+use File::Temp;
+
+# our $login='admin';
+# our $pass='adminpass';
+# our $url='http://localhost/sugarcrm/service/v4/rest.php';
+# our $testcampaign='Demo Campaign';
+# our $testprospectlist='Demo users';
+# our $testemailmarketing='Demo User send password';
+# # These are needed only to send emails in campaigns
+# our $testdsn='DBI:mysql:database=theqvdsugarcrm;host=db.prepro.qindel.com';
+# our $testdbuser='sugarcrm';
+# our $testdbpass='pass';
+# our $testemail1='myemail1@mydomain.org';
+# our $testemail2='myemail2@mydomain.org';
